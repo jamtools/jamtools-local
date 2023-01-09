090727a1c563c37f0919896fc69b50d1994f2f5c
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2023-01-09 00:53:21 -0500

    initial wled and midi code (#1)
    - nanokey studio controls juno
    - and controls wled with knobs and buttons

---
+++ src/scratch.ts
@@ -0,0 +1,18 @@
+const easymidi = require('easymidi');
+
+import {INPUT_TYPES, INPUT_EXTENDED_TYPES} from './constants/easymidi_constants';
+
+const midiInputName = 'Juno USB Midi';
+const input = new easymidi.Input(midiInputName);
+
+// input.on('noteon', console.log);
+
+INPUT_TYPES.concat(INPUT_EXTENDED_TYPES).forEach(type => {
+    input.on(type, msg => {
+        if (['noteon', 'noteoff'].includes(type) && msg.channel === 0) {
+            return;
+        }
+
+        console.log(msg);
+    });
+});
