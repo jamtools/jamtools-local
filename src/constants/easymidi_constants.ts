ba096c1cf821a197f7fee19bfe7b1d853acee359
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2023-01-09 00:46:04 -0500

    initial wled and midi code - nanokey studio controls juno - and controls wled with knobs and buttons

---
+++ src/constants/easymidi_constants.ts
@@ -0,0 +1,29 @@
+export const INPUT_TYPES = [
+    'noteoff',
+    'noteon',
+    'poly aftertouch',
+    'cc',
+    'program',
+    'channel aftertouch',
+    'pitch',
+];
+
+export const INPUT_EXTENDED_TYPES = [
+    'sysex',
+    'mtc',
+    'position',
+    'select',
+    'tune',
+    'sysex end',
+    'clock',
+    'start',
+    'continue',
+    'stop',
+    'activesense',
+    'reset'
+];
+
+export const SPAMMY_MIDI_EVENT_TYPES = {
+    CLOCK: 'clock',
+    PITCH: 'pitch',
+}
